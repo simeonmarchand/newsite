d5198dee8e8afe3e9b01f0385e8f05c89469042e
Simeon Marchand <simeonrmarchand@gmail.com>
2023-04-29 11:11:46 -0500

    filling the where table

--- query_where.php
+++ query_where.php
@@ -1,25 +1,29 @@
 <?php // query_dashboard.php
 
+error_reporting(E_ALL | E_STRICT);
+ini_set("display_errors", "1");
+ini_set("display_startup_errors", "1");
+ini_set("html_errors", "1");
+ini_set("docref_root", "http://www.php.net/");
+ini_set("error_prepend_string", "<div style='color:red; font-family:verdana; border:1px solid red; padding:5px;'>");
+ini_set("error_append_string", "</div>");
+
   require_once 'login.php';
   $connection = new mysqli ($hn, $un, $pw, $db);
   if ($connection->connect_error) die('Could not connect to the DB');
 
-  $query = "SELECT contacts.contact_first, contacts.contact_last, contacts_audits.contacts_id, contacts_audits.change_date
-            FROM contacts
-            JOIN contacts_audit ON contacts.contact_id = contacts_audits.contact_id
-            WHERE contacts.contact_last = 'Allen'
-            AND contacts_audits.change_date >= '2023-04-23'";
+  $query = "SELECT * FROM contacts WHERE company LIKE '%Creative%' AND industry = 'photography'";
   $result = $connection->query($query);
 
   if (!$result) die('No query result!');
 
   echo '<html>
-  <head><title>Contacts</title></head>
+  <head><title>Where Table</title></head>
   <body>
-  <h1>Contact Info</h1>';
+  <h1>Where Table</h1>';
 
   // Execute the SQL query
-$result = $mysqli->query($query);
+$result = $connection->query($query);
 
 // Check if there's an error executing the query
 if (!$result) {
@@ -27,13 +31,23 @@ if (!$result) {
     exit();
 }
 
+$rows = $result->num_rows;
+
 // Loop through the query results and output the data
-while ($row = $result->fetch_assoc()) {
-    echo $row['contact_first'] . ' ' . $row['contact_last'] . ' ' . $row['contacts_id'] . ' ' . $row['change_date'] . '<br>';
+for ($j = 0 ; $j < $rows ; ++$j){
+  $result->data_seek($j);
+  echo 'Company: '
+    .htmlspecialchars($result->fetch_assoc()['company'])
+    .'<br>';
+    $result->data_seek($j);
+  echo 'Industry: '
+    .htmlspecialchars($result->fetch_assoc()['industry'])
+    .'<br>';
+    $result->data_seek($j);
 }
 
-// Close the database connection
-$mysqli->close();
+$result->close();
+$connection->close();
 
-  echo '</body></html>';
+echo '</body></html>';
 ?>
